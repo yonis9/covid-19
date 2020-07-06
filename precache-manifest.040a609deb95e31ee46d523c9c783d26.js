120b5391804e1bb57df986dbfce81a2d51bc5c2a
Yoni Sisso <yonis9@walla.com>
2020-07-06 19:54:41 +0300

    Updates

---
+++ precache-manifest.040a609deb95e31ee46d523c9c783d26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d611eb9455b854c03f6d11fb954e8d3",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "c7d529467b2dac7330f6",
+    "url": "/covid-19/static/css/main.44c40f31.chunk.css"
+  },
+  {
+    "revision": "ea399201cf259c431d5e",
+    "url": "/covid-19/static/js/2.18ceda98.chunk.js"
+  },
+  {
+    "revision": "0fb0e672f1308cb5b7677cdb93fc42ab",
+    "url": "/covid-19/static/js/2.18ceda98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7d529467b2dac7330f6",
+    "url": "/covid-19/static/js/main.7e5b45ad.chunk.js"
+  },
+  {
+    "revision": "ca6f7991abbe432f93f7",
+    "url": "/covid-19/static/js/runtime-main.8e18646f.js"
+  }
+]);
